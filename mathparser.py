6cdb62a9e7e768bb320e8e026d254b4611e7e143
Exzees <f3wll214qdtsfbadwfa@mail.ru>
2025-09-05 13:24:41 +0300

    naming

--- mathparser.py
+++ mathparser.py
@@ -13,17 +13,19 @@ opperations = {
     "%": lambda x, y: x % y,
 }
 
-def math_parser(messege:str, json_log:bool = True) -> float:
+def math_parser(messege:str, json_log:bool = True,
+                filePath:Path = Path(__file__).parent) -> float:
     
     filename = "user-math-story.json"
     number_slots = []
     operator_slots = []
     NUMBERS_CHARS  = '1234567890.'
+    OPERATIONS_CHARS = ''.join(opperations.keys())
     num = ''
     last_operator = '+'
     
     for i, item in enumerate(messege):
-        if item in NUMBERS_CHARS  + ''.join(opperations.keys()):
+        if item in NUMBERS_CHARS  + OPERATIONS_CHARS:
             if item in NUMBERS_CHARS :
                 if item == '.' and item in num:
                     continue
@@ -31,7 +33,7 @@ def math_parser(messege:str, json_log:bool = True) -> float:
                     operator_slots.append(last_operator)
                 num += item
                 
-            if item in opperations.keys():
+            if item in OPERATIONS_CHARS:
                 if messege[i] == messege[i-1] and item in "*/" and i > 0:
                     last_operator = item * 2
                 else:
@@ -83,7 +85,7 @@ def math_parser(messege:str, json_log:bool = True) -> float:
         data_slot['messege_encoding'] = ''.join(f'{x,y}' for x, y in zip(operator_slots, number_slots)).strip()
         data_slot['result'] = result
         
-        filePath = Path(__file__).parent / filename
+        filePath =  filePath / filename
         if filePath.exists():
             with open(filePath, mode="r", encoding='utf-8') as file:
                 try:
